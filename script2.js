dcf00f4a581cae837cfb3f3abbbb18d331add544
Aman sharma <109153961+Aman-Sharma-au49@users.noreply.github.com>
2023-12-16 17:18:44 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ function nextquiz(event) {
 
     var nameInput = document.querySelector('input[type="text"]');
     var emailInput = document.querySelector('input[type="email"]');
-    var phoneInput = document.querySelector('input[type="text"]');
+    var phoneInput = document.querySelector('input[type="tel"]');
 
     if (nameInput.value.trim() === "" || phoneInput.value.trim() === "" || emailInput.value.trim() === "") {
         alert("Please fill out all required fields.");
